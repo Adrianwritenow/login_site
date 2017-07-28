af628ed3cd172ac9a2d850af63bd9de158d5af6c
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-28 09:22:45 -0400

    finished and added logout button

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ app.use(session({
 
 app.get('/', function(req,res){
   if (req.session && req.session.admin){
-    let currentUser = req.body.userName;
-    res.send( currentUser+" is logged in.");
-    console.log(req.body);
+    res.send(req.session.userName+` is logged in.<form action="/" method="post"><button>log out</button>`);
   }else{
     res.redirect('/login');
   }
@@ -45,30 +43,19 @@ app.get('/login',function(req,res){
 
 });
 
-
-console.log("2");
-
-
 app.post('/login', function(req,res){
     if (req.body.userPass === usersKey[req.body.userName]) {
+     req.session.userName  = req.body.userName
       req.session.admin = true;
-      console.log("3");
       res.redirect('/');
-
   }else{
-    res.redirect('/');
+    res.send(` Please try again <form action="/" method="post"><button>Try Again</button>`);
   }
-
-  // console.log(usersKey[req.body.userPass]);
-
 });
-
 
 let auth = function(req,res,next){
   if (req.session && req.session.admin){
     console.log("4");
-
-
     return next();
   }else{
     return res.sendStatus(401);
@@ -78,6 +65,10 @@ app.get('/', auth, function(req,res){
   res.redirect('/login');
   console.log("5");
 });
+app.post('/', function(req,res){
+  req.session.destroy();
+  res.render('login');
+})
 app.listen(3000, function(){
   console.log('server farted');
 
